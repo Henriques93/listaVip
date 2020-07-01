d8b4e83aa3e7a36d3a5cf6bf2b7264e302ab8cdc
Henriques <henriquesxp@gmail.com>
2020-06-30 22:24:34 -0300

    Criando Projeto

---
+++ src/main/java/br/com/alura/listavip/ListavipApplication.java
@@ -0,0 +1,15 @@
+package br.com.alura.listavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class ListavipApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ListavipApplication.class, args);
+	}
+
+}
